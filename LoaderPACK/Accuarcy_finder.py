b6e29dab8a63aba36a47bdeb32a2a4684b84fd6e
Javert-ctrl <79468558+marctimjen@users.noreply.github.com>
2022-03-21 11:27:26 +0100

    Update Accuarcy_finder.py

--- LoaderPACK/Accuarcy_finder.py
+++ LoaderPACK/Accuarcy_finder.py
@@ -14,15 +14,15 @@ def Accuarcy_find(pred, tar, device):
     tar = tar.view(-1, 5*60*250)
     art_pred = pred[:, 1] >= 0.5 # what the model predicts as artefacts
 
-    fp = torch.sum(art_pred[tar == 0] == 1) # false positive, tar = 0 and pred = 1
-    fn = torch.sum(art_pred[tar == 1] == 0) # false negative, tar = 1 and pred = 0
+    fp = torch.sum(art_pred[tar == 0] == True) # false positive, tar = 0 and pred = 1
+    fn = torch.sum(art_pred[tar == 1] == False) # false negative, tar = 1 and pred = 0
 
-    tp = torch.sum(art_pred[tar == 1] == 1) # true positive
-    tn = torch.sum(art_pred[tar == 0] == 0) # true negative
+    tp = torch.sum(art_pred[tar == 1] == True) # true positive
+    tn = torch.sum(art_pred[tar == 0] == False) # true negative
 
     acc = (fp + fn)/(fp + fn + tp + tn)
 
     tot_p = tp + fp # total postive
     tot_n = tn + fn # total negative
-    
+
     return (acc, torch.tensor([[tp, fp], [fn, tn]]), tot_p, tot_n)
