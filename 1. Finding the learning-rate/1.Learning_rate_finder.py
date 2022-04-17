45f8740a1d5b5cf56d295967bc304158fd1d123e
Javert-ctrl <79468558+marctimjen@users.noreply.github.com>
2022-04-17 13:51:05 +0200

    test

--- 1. Finding the learning-rate/1.Learning_rate_finder.py
+++ 1. Finding the learning-rate/1.Learning_rate_finder.py
@@ -333,6 +333,8 @@ if __name__ == '__main__':
                                                     shuffle=True,
                                                     num_workers=0)
 
+    for i in train_file_loader:
+        print(i)
 
     val_set = random.sample(range(1, 28 + 1), 20)
 
