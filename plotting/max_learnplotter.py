1032c8a0051bdbf34fa469409b6d6a53e40f5e85
Javert-ctrl <79468558+marctimjen@users.noreply.github.com>
2022-05-07 14:11:39 +0200

    adam is difficult to tune

--- plotting/max_learnplotter.py
+++ plotting/max_learnplotter.py
@@ -6,7 +6,7 @@ token = os.getenv('Neptune_api')
 run = neptune.init(
     project="NTLAB/artifact-rej-scalp",
     api_token=token,
-    run="AR1-269"
+    run="AR1-270"
 ) # adam network - AR1-259
 
 # AR1-263
