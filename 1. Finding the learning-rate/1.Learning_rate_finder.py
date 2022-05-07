d822d526f36a00a4fadac4f4d7f61e298681ac16
Javert-ctrl <79468558+marctimjen@users.noreply.github.com>
2022-05-07 15:02:59 +0200

    new try

--- 1. Finding the learning-rate/1.Learning_rate_finder.py
+++ 1. Finding the learning-rate/1.Learning_rate_finder.py
@@ -214,7 +214,7 @@ def net_ADAM1(device, fl, it, train_path, val_path):
     avg_train_loss, avg_valid_loss = [], []
 
     model = Unet_leaky(n_channels=1, n_classes=2).to(device)
-    optimizer = Adam(model.parameters(), lr=0.0000000000001, weight_decay = 0)
+    optimizer = Adam(model.parameters(), lr=0.0000000000001)
     lossFunc = nn.CrossEntropyLoss(weight = torch.tensor([1., 5.]).to(device),
                                    reduction = "mean")
 
