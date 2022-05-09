236427b64665f13eead111aa70acfc76686b7c85
Javert-ctrl <79468558+marctimjen@users.noreply.github.com>
2022-05-09 12:26:56 +0200

    change the test of loader to py

---
+++ Testing loader/.ipynb_checkpoints/test-loader-checkpoint.py
@@ -0,0 +1,24 @@
+import csv
+import re
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+sys.path.append("..") # adds higher directory to python modules path
+
+from LoaderPACK.Loader import testload_5min
+import torch
+
+val_load_file = testload_5min(path = "/home/tyson/data_cutoff/val_model_data",
+                                     series_dict = 'val_series_length.pickle',
+                                     size = (28, 22, 549200),
+                                     device = "cpu")
+
+
+val_loader = torch.utils.data.DataLoader(val_load_file,
+                                                batch_size=1,
+                                                shuffle=True,
+                                                num_workers=0)
+
+for i in val_loader:
+    print(i)
