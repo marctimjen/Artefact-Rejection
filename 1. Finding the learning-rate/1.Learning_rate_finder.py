631134ba8b767c57427917e79d069b791697eee2
marctimjen <79468558+marctimjen@users.noreply.github.com>
2022-05-13 16:05:02 +0200

    Making the script ready for deployment

--- 1. Finding the learning-rate/1.Learning_rate_finder.py
+++ 1. Finding the learning-rate/1.Learning_rate_finder.py
@@ -34,7 +34,7 @@ def net_SGD1(device, fl, it, train_path, val_path):
     )
 
     batch_size = 10
-    n_samples = 50 # how many samples do we collect
+    n_samples = 500 # how many samples do we collect
 
     train_load_file = shuffle_5min(path = train_path,
                                    series_dict = 'train_series_length.pickle',
@@ -204,7 +204,7 @@ def net_SGD2(device, fl, it, train_path, val_path):
     )
 
     batch_size = 10
-    n_samples = 50 # how many samples do we collect
+    n_samples = 500 # how many samples do we collect
 
     train_load_file = shuffle_5min(path = train_path,
                                    series_dict = 'train_series_length.pickle',
@@ -371,7 +371,7 @@ def net_ADAM1(device, fl, it, train_path, val_path):
     )
 
     batch_size = 10
-    n_samples = 50 # how many samples do we collect
+    n_samples = 500 # how many samples do we collect
 
     train_load_file = shuffle_5min(path = train_path,
                                    series_dict = 'train_series_length.pickle',
@@ -542,7 +542,7 @@ def net_ADAM2(device, fl, it, train_path, val_path):
     )
 
     batch_size = 10
-    n_samples = 50 # how many samples do we collect
+    n_samples = 500 # how many samples do we collect
 
     train_load_file = shuffle_5min(path = train_path,
                                    series_dict = 'train_series_length.pickle',
